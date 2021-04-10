1131dad3f42f329d44521f6257a1dc8ccaa6e583
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-04-10 13:01:18 +0200

    few components and HomeScreen added

---
+++ src/products.js
@@ -0,0 +1,52 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Plaster dębowy',
+        image: './images/oakWithoutBark.jpg',
+        description: 'Plaster dębowy szlifowany bez kory',
+        diameter: '30 - 35 cm',
+        price: '54.99 PLN',
+        category: 'plastry',
+        rating: '5',
+        numReviews: '12',
+        countInStock: '1',
+    },
+    {
+        _id: '2',
+        name: 'Plaster dębowy',
+        image: './images/oakWithBark.jpg',
+        description: 'Plaster dębowy szlifowany z korą',
+        diameter: '30 - 35 cm',
+        price: '44.99 PLN',
+        category: 'plastry',
+        rating: '5',
+        numReviews: '20',
+        countInStock: '1',
+    },
+    {
+        _id: '3',
+        name: 'Stolik',
+        image: './images/oakTable.jpg',
+        description: 'Stolik rustykalny z plastra dębowego',
+        diameter: '30 - 35 cm',
+        price: '449 PLN',
+        category: 'meble',
+        rating: '5',
+        numReviews: '6',
+        countInStock: '1',
+    },
+    {
+        _id: '4',
+        name: 'Plaster dębowy',
+        image: './images/oakWithPolishedBark.jpg',
+        description: 'Plaster dębowy szlifowany z korą szczotkowaną',
+        diameter: '30 - 35 cm',
+        price: '54.99 PLN',
+        category: 'plastry',
+        rating: '5',
+        numReviews: '14',
+        countInStock: '1',
+    }
+]
+
+export default products
