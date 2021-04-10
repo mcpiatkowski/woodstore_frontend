24d42057ae60665a42e2fdff2baaca4f2a6098cd
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-04-10 18:14:15 +0200

    product screen added

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,9 +1,76 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-function ProductScreen() {
+
+function ProductScreen({ match }) {
+    const product = products.find(p => p._id === match.params.id)
     return (
         <div>
-            Product
+            <Link to="/" className="btn btn-light my-3">Go back</Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={ product.image } alt={product.name} fluid />
+                </Col>
+
+                <Col md={3}>
+                    <ListGroup variant="flush">
+
+                        <ListGroup.Item>
+                            <h3>{ product.name }</h3>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews} recenzji`} color={'#f8e825'} />
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            Cena: { product.price }
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            Opis: { product.description }
+                        </ListGroup.Item>
+
+                    </ListGroup>
+                </Col>
+
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant="flush">
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Price:
+                                    </Col>
+                                    <Col md="auto">
+                                        <strong>{product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Status:
+                                    </Col>
+                                    <Col md="auto">
+                                        {product.countInStock > 0 ? 'W magazynie' : 'Brak'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Button className="btn-block" disabled={product.countInStock === '0'} type="button">Do koszyka</Button>
+                            </ListGroup.Item>
+
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
