bc9546738b54a11a24231c94d72c486516df1c30
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-04-22 18:49:50 +0200

    profile update

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -4,7 +4,8 @@ import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUserProfile } from "../actions/userActions";
+import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants";
 
 function ProfileScreen({ history }) {
   const [name, setName] = useState("");
@@ -19,21 +20,27 @@ function ProfileScreen({ history }) {
   const userDetails = useSelector((state) => state.userDetails);
   const { error, loading, user } = userDetails;
 
+  // @ts-ignore
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
+  // @ts-ignore
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
 
   useEffect(() => {
     if (!userInfo) {
       history.push("/login");
     } else {
-      if (!user || !user.name) {
+      if (!user || !user.name || success) {
+        dispatch({ type: USER_UPDATE_PROFILE_RESET });
         dispatch(getUserDetails("profile"));
       } else {
         setName(user.name);
         setEmail(user.email);
       }
     }
-  }, [dispatch, history, userInfo, user]);
+  }, [dispatch, history, userInfo, user, success]);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -41,7 +48,15 @@ function ProfileScreen({ history }) {
     if (password !== confirmPassword) {
       setMessage("Hasła nie są zgodne.");
     } else {
-      console.log("YO!");
+      dispatch(
+        updateUserProfile({
+          id: user._id,
+          name: name,
+          email: email,
+          password: password,
+        })
+      );
+      setMessage("Hasło zmienione.");
     }
   };
 
@@ -80,9 +95,8 @@ function ProfileScreen({ history }) {
           <Form.Group controlId='password'>
             <Form.Label>Hasło</Form.Label>
             <Form.Control
-              required
               type='password'
-              placeholder='Twoje hasło'
+              placeholder='Nowe hasło'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             ></Form.Control>
@@ -91,7 +105,6 @@ function ProfileScreen({ history }) {
           <Form.Group controlId='passwordConfirm'>
             <Form.Label>Potwierdź hasło</Form.Label>
             <Form.Control
-              required
               type='password'
               placeholder='Potwierdź hasło'
               value={confirmPassword}
@@ -100,7 +113,7 @@ function ProfileScreen({ history }) {
           </Form.Group>
 
           <Button type='submit' variant='primary'>
-            Zarejestruj się
+            Aktualizuj
           </Button>
         </Form>
       </Col>
