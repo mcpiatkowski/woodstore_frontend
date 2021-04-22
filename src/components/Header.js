51fecf5d7572eb94d141423b4a55da42430b4052
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-04-22 12:27:00 +0200

    user register

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,57 @@
-import React from 'react'
-import { Nav, Navbar, Container, Row } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Nav, Navbar, Container, Row, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { logout } from "../actions/userActions";
 
 function Header() {
-    return (
-        <header>
-          <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-            <Container>
-              <LinkContainer to="/">
-                <Navbar.Brand>Design by nature</Navbar.Brand>
+  // @ts-ignore
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Design by nature</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='mr-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i>Koszyk
+                </Nav.Link>
               </LinkContainer>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <LinkContainer to="/cart">
-                  <Nav.Link><i className="fas fa-shopping-cart"></i>Koszyk</Nav.Link>
+
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profil</NavDropdown.Item>
+                  </LinkContainer>
+
+                  <NavDropdown.Item onClick={logoutHandler}>Wyloguj</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'></i>Login
+                  </Nav.Link>
                 </LinkContainer>
-                <LinkContainer to="/login">
-                  <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
-                </LinkContainer>
-              </Nav>
-              </Navbar.Collapse>
-            </Container>   
-          </Navbar>
-        </header>
-    )
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
